87ba7665b9500cc452dfab8efbbab6a3d5c6acd3
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-06-10 09:12:27 -0400

    Adds jumbotron on homepage that links to collection.

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./home.styles.scss";
 import Directory from "../../components/directory/directory.component";
+import Jumbotron from "../../components/jumbotron/jumbotron.component";
 
 export const Home = () => {
   return (
     <div className="home">
+      <Jumbotron />
+      <h2>Browse By Category</h2>
       <Directory />
     </div>
   );
