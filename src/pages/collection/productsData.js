6e129ce5f0355d4699174e644d30af0c94ff139d
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-06-11 09:48:04 -0400

    Changes productData.js to reflect theme of site. Adds dummyimage for all products.

--- src/pages/collection/productsData.js
+++ src/pages/collection/productsData.js
@@ -6,57 +6,33 @@ const SHOP_DATA = [
     items: [
       {
         id: 1,
-        name: "Brown Brim",
-        imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
-        price: 25
+        name: 'Samsung 32" 60Hz 4ms Curved LED',
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 349.99
       },
       {
         id: 2,
-        name: "Blue Beanie",
-        imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
-        price: 18
+        name: 'Acer 31.5" FHD 75Hz 4ms Curved Gaming Monitor',
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 299.99
       },
       {
         id: 3,
-        name: "Brown Cowboy",
-        imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
-        price: 35
+        name: 'Asus 27" FHD 60Hz 5ms LED',
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 209.91
       },
       {
         id: 4,
-        name: "Grey Brim",
-        imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
+        name: 'LG 29" Ultrawide FHD 75Hz 5ms Gaming Monitor',
+        imageUrl: "https://dummyimage.com/400x400",
         price: 25
       },
       {
         id: 5,
-        name: "Green Beanie",
-        imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
-        price: 18
-      },
-      {
-        id: 6,
-        name: "Palm Tree Cap",
-        imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
-        price: 14
-      },
-      {
-        id: 7,
-        name: "Red Beanie",
-        imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
-        price: 18
-      },
-      {
-        id: 8,
-        name: "Wolf Cap",
-        imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
-        price: 14
-      },
-      {
-        id: 9,
-        name: "Blue Snapback",
-        imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
-        price: 16
+        name: 'Dell 24" SE2419HR',
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 169.99
       }
     ]
   },
@@ -66,232 +42,185 @@ const SHOP_DATA = [
     routeName: "laptops",
     items: [
       {
+        id: 6,
+        name:
+          'Macbook Pro (2020) 13.3" (Intel Core i3 1.1GHZ/256GB SSD/8GB RAM)',
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 1299.99
+      },
+      {
+        id: 7,
+        name: "Dell XPS 15 2020",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 1800.99
+      },
+      {
+        id: 8,
+        name: 'Acer Aspire 5 15.6" (Intel Core i5/512GB SSD/12GB RAM)',
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 742.65
+      },
+      {
+        id: 9,
+        name: 'ASUS ZenBook 14" (AMD Quad Core R5-3500U/512GB SSD/8GB RAM)',
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 799.99
+      },
+      {
         id: 10,
-        name: "Promaster Diver",
-        imageUrl:
-          "https://embed.widencdn.net/img/citizenwatch/1db5a7ycwb/1000px/Promaster%20Diver.png?u=41zuoe",
-        price: 400
-      },
-      {
-        id: 11,
-        name: "Cluse",
-        imageUrl:
-          "https://images.unsplash.com/photo-1506796684999-9fa2770af9c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
-        price: 280
-      },
-      {
-        id: 12,
-        name: "Fossil",
-        imageUrl:
-          "https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        price: 110
-      },
-      {
-        id: 13,
-        name: "MVMT",
-        imageUrl:
-          "https://images.unsplash.com/photo-1495857000853-fe46c8aefc30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        price: 160
-      },
-      {
-        id: 14,
-        name: "Citizen",
-        imageUrl:
-          "https://images.unsplash.com/photo-1511370235399-1802cae1d32f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1037&q=80",
-        price: 160
-      },
-      {
-        id: 15,
-        name: "Nike Brown High Tops",
-        imageUrl:
-          "https://images.unsplash.com/photo-1507428861205-e8aab693190e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1060&q=80",
-        price: 160
-      },
-      {
-        id: 16,
-        name: "Rolex",
-        imageUrl:
-          "https://images.unsplash.com/photo-1526045431048-f857369baa09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        price: 190
-      },
-      {
-        id: 17,
-        name: "Timberlands",
-        imageUrl:
-          "https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-        price: 200
+        name: "Surface Book 3",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 1234.32
       }
     ]
   },
   {
     id: 3,
     title: "Storage Devices",
-    routeName: "jackets",
+    routeName: "storage",
     items: [
       {
-        id: 18,
-        name: "Black Jean Shearling",
-        imageUrl: "https://i.ibb.co/XzcwL5s/black-shearling.png",
-        price: 125
+        id: 11,
+        name: "Seagate Backup Plus Slim 2TB USB 3.0",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 89.99
       },
       {
-        id: 19,
-        name: "Blue Jean Jacket",
-        imageUrl: "https://i.ibb.co/mJS6vz0/blue-jean-jacket.png",
-        price: 90
+        id: 12,
+        name: "Lexar JumpDrive S50 64GB USB Flash Drive",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 19.99
       },
       {
-        id: 20,
-        name: "Grey Jean Jacket",
-        imageUrl: "https://i.ibb.co/N71k1ML/grey-jean-jacket.png",
-        price: 90
+        id: 13,
+        name: "Philips Vivid 32GB USB 3.0 Flash Drive (3 Pack)",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 24.99
       },
       {
-        id: 21,
-        name: "Brown Shearling",
-        imageUrl: "https://i.ibb.co/s96FpdP/brown-shearling.png",
-        price: 165
+        id: 14,
+        name: "Samsung T7 Touch Portable 500GB USB External SSD",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 174.99
       },
       {
-        id: 22,
-        name: "Tan Trench",
-        imageUrl: "https://i.ibb.co/M6hHc3F/brown-trench.png",
-        price: 185
+        id: 15,
+        name: "SanDisk Extreme 500GB USB External SSD",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 139.99
       }
     ]
   },
   {
     id: 4,
     title: "Drives",
-    routeName: "womens",
+    routeName: "internaldrives",
     items: [
       {
-        id: 23,
-        name: "Blue Tanktop",
-        imageUrl: "https://i.ibb.co/7CQVJNm/blue-tank.png",
-        price: 25
+        id: 16,
+        name: "WD Desktop Hard Disk Drive 4TB",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 119.99
       },
       {
-        id: 24,
-        name: "Floral Blouse",
-        imageUrl: "https://i.ibb.co/4W2DGKm/floral-blouse.png",
-        price: 20
+        id: 17,
+        name: "Seagate BarraCuda 2TB SATA 7200RPM HDD",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 74.99
       },
       {
-        id: 25,
-        name: "Floral Dress",
-        imageUrl: "https://i.ibb.co/KV18Ysr/floral-skirt.png",
-        price: 80
+        id: 18,
+        name: "Samsung 860 EVO Series 500GB SATA III SSD",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 128.5
       },
       {
-        id: 26,
-        name: "Red Dots Dress",
-        imageUrl: "https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png",
-        price: 80
+        id: 19,
+        name: "HP S600 120GB SATA III 3D NAND SSD",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 32.99
       },
       {
-        id: 27,
-        name: "Striped Sweater",
-        imageUrl: "https://i.ibb.co/KmSkMbH/striped-sweater.png",
+        id: 20,
+        name: "Seagate BarraCuda 1TB 7200RPM HDD",
+        imageUrl: "https://dummyimage.com/400x400",
         price: 45
-      },
-      {
-        id: 28,
-        name: "Yellow Track Suit",
-        imageUrl: "https://i.ibb.co/v1cvwNf/yellow-track-suit.png",
-        price: 135
-      },
-      {
-        id: 29,
-        name: "White Blouse",
-        imageUrl: "https://i.ibb.co/qBcrsJg/white-vest.png",
-        price: 20
       }
     ]
   },
   {
     id: 5,
     title: "Desktop Computers",
-    routeName: "mens",
+    routeName: "desktops",
     items: [
       {
-        id: 30,
-        name: "Camo Down Vest",
-        imageUrl: "https://i.ibb.co/xJS0T3Y/camo-vest.png",
-        price: 325
+        id: 21,
+        name: "Dell Inspiron - 9th gen Intel Core/8GB RAM/1TB HDD",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 499.99
       },
       {
-        id: 31,
-        name: "Floral T-shirt",
-        imageUrl: "https://i.ibb.co/qMQ75QZ/floral-shirt.png",
-        price: 20
+        id: 22,
+        name:
+          "Alienware Aurora R11 Gaming Desktop - 10th gen Intel Core/8GB RAM/1TB HDD",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 1299.99
       },
       {
-        id: 32,
-        name: "Black & White Longsleeve",
-        imageUrl: "https://i.ibb.co/55z32tw/long-sleeve.png",
-        price: 25
+        id: 23,
+        name: "ASUS S340MF - Intel Core i5-9400/1TB HDD/256GB SSD/8GB RAM",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 799.99
       },
       {
-        id: 33,
-        name: "Pink T-shirt",
-        imageUrl: "https://i.ibb.co/RvwnBL8/pink-shirt.png",
-        price: 25
+        id: 24,
+        name: "Acer Aspire TC - Intel Core Ci5-9400/1TB HDD/12GB RAM",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 719.99
       },
       {
-        id: 34,
-        name: "Jean Long Sleeve",
-        imageUrl: "https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png",
-        price: 40
-      },
-      {
-        id: 35,
-        name: "Burgundy T-shirt",
-        imageUrl: "https://i.ibb.co/mh3VM1f/polka-dot-shirt.png",
-        price: 25
+        id: 25,
+        name:
+          "Dell Vostro DT 3471 Business Desktop - 9th gen Intel Core/4GB RAM/1TB HDD",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 529.42
       }
     ]
   },
   {
     id: 6,
     title: "Keyboards & Mice",
-    routeName: "mens",
+    routeName: "keyboardsmice",
     items: [
       {
+        id: 26,
+        name: "Surface Pro Signature Type Cover",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 159.99
+      },
+      {
+        id: 27,
+        name: "Microsoft Designer Bluetooth",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 99.99
+      },
+      {
+        id: 28,
+        name: "Razer Deathadder Elite Ergonomic Gaming Mouse",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 54.99
+      },
+      {
+        id: 29,
+        name: "Razer Cynosa Chroma Backlit Gaming Keyboard",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 59.99
+      },
+      {
         id: 30,
-        name: "Camo Down Vest",
-        imageUrl: "https://i.ibb.co/xJS0T3Y/camo-vest.png",
-        price: 325
-      },
-      {
-        id: 31,
-        name: "Floral T-shirt",
-        imageUrl: "https://i.ibb.co/qMQ75QZ/floral-shirt.png",
-        price: 20
-      },
-      {
-        id: 32,
-        name: "Black & White Longsleeve",
-        imageUrl: "https://i.ibb.co/55z32tw/long-sleeve.png",
-        price: 25
-      },
-      {
-        id: 33,
-        name: "Pink T-shirt",
-        imageUrl: "https://i.ibb.co/RvwnBL8/pink-shirt.png",
-        price: 25
-      },
-      {
-        id: 34,
-        name: "Jean Long Sleeve",
-        imageUrl: "https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png",
-        price: 40
-      },
-      {
-        id: 35,
-        name: "Burgundy T-shirt",
-        imageUrl: "https://i.ibb.co/mh3VM1f/polka-dot-shirt.png",
-        price: 25
+        name: "Surface Mobile Mouse",
+        imageUrl: "https://dummyimage.com/400x400",
+        price: 44.99
       }
     ]
   }
