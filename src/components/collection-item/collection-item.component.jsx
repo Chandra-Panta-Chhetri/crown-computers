0add1502aa83e5509e11f73f6dff0c505d7e4edf
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-06-16 08:41:46 -0400

    Adds add to cart btn on product img hover.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./collection-item.styles.scss";
 
+import { Button } from "../button/button.component";
+
 export const CollectionItem = ({ name, category, imageUrl, price }) => (
   <article className="collection-item">
-    <img className="image" src={imageUrl} alt={`${name}`} />
+    <div className="img-container">
+      <img className="product-img" src={imageUrl} alt={name} />
+      <Button onClick={() => console.log("add btn clicked")}>
+        <i className="fas fa-cart-plus"></i> Add To Cart
+      </Button>
+    </div>
     <div className="body">
       <h5 className="category">{category}</h5>
       <h4 className="name">{name}</h4>
