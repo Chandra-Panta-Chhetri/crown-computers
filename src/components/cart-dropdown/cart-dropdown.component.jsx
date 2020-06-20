5120bc89e7b79fefe42b4ae9347fe649ba759887
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-06-20 11:36:04 -0400

    On view cart button click, hides cart drop-down.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,8 +7,9 @@ import { CartItem } from "../cart-item/cart-item.component";
 import { connect } from "react-redux";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { withRouter } from "react-router-dom";
+import { toggleCartVisibility } from "../../redux/cart/cart.actions";
 
-const CartDropDown = ({ shoppingCart, history }) => (
+const CartDropDown = ({ shoppingCart, history, dispatch }) => (
   <article className="cart-drop-down">
     <div className="cart-items">
       {!shoppingCart.length ? (
@@ -17,7 +18,14 @@ const CartDropDown = ({ shoppingCart, history }) => (
         shoppingCart.map((item) => <CartItem key={item.id} item={item} />)
       )}
     </div>
-    <Button onClick={() => history.push("/checkout")}>View Cart</Button>
+    <Button
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toggleCartVisibility());
+      }}
+    >
+      View Cart
+    </Button>
   </article>
 );
 
