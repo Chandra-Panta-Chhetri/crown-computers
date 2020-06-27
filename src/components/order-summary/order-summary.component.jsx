fe6c4e356a1f82814a59e0e3f751c778fb6630ad
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-06-27 11:37:27 -0400

    Adds order summary component. Removes Your Cart Summary heading.

---
+++ src/components/order-summary/order-summary.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./order-summary.styles.scss";
+
+import { Button } from "../button/button.component";
+
+export const OrderSummary = ({ cartTotal }) => (
+  <article className="order-summary">
+    <h2 className="order-summary-heading">Order Summary</h2>
+    <section className="price-breakdown">
+      <div>
+        Subtotal <span>${cartTotal}</span>
+      </div>
+      <div>
+        Total Tax <span>${Math.round(cartTotal * 0.13 * 100) / 100}</span>
+      </div>
+      <div>
+        Total <span>${Math.round(cartTotal * 1.13 * 100) / 100}</span>
+      </div>
+    </section>
+    <Button>Checkout</Button>
+  </article>
+);
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./checkout.styles.scss";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import { OrderSummary } from "../../components/order-summary/order-summary.component";
 
 import { connect } from "react-redux";
 import {
@@ -11,13 +12,14 @@ import {
 import { createStructuredSelector } from "reselect";
 
 const CheckOut = ({ cartItems, cartTotal }) => (
-  <div className="checkout-summary">
-    <h3>Your Cart Summary</h3>
-    {cartItems.map((cartItem) => (
-      <CheckoutItem key={cartItem.id} item={cartItem} />
-    ))}
-    <h4 className="cart-total">${cartTotal}</h4>
-  </div>
+  <article className="checkout-summary">
+    <section className="checkout-items">
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} item={cartItem} />
+      ))}
+    </section>
+    {cartItems.length ? <OrderSummary cartTotal={cartTotal} /> : null}
+  </article>
 );
 
 const mapStateToProps = createStructuredSelector({
