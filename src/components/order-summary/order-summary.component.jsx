718b73b2e6035504a4b38157df685b1e3babc6cd
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-07-08 11:04:06 -0400

    Adds stripe checkout button component and paying functionality with stripe.

--- src/components/order-summary/order-summary.component.jsx
+++ src/components/order-summary/order-summary.component.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import "./order-summary.styles.scss";
 
-import { Button } from "../button/button.component";
+import StripeCheckoutButton from "../stripe-checkout-button/stripe-checkout-button.component";
 
-export const OrderSummary = ({ cartTotal }) => (
-  <article className="order-summary">
-    <h2 className="order-summary-heading">Order Summary</h2>
-    <section className="price-breakdown">
-      <div>
-        Subtotal <span>${Math.round(cartTotal * 100) / 100}</span>
-      </div>
-      <div>
-        Total Tax <span>${Math.round(cartTotal * 0.13 * 100) / 100}</span>
-      </div>
-      <div>
-        Total <span>${Math.round(cartTotal * 1.13 * 100) / 100}</span>
-      </div>
-    </section>
-    <Button>Checkout</Button>
-  </article>
-);
+export const OrderSummary = ({ cartTotal }) => {
+  const subtotal = Math.round(cartTotal * 100) / 100;
+  const totalTax = Math.round(cartTotal * 0.13 * 100) / 100;
+  const total = Math.round(cartTotal * 1.13 * 100) / 100;
+  return (
+    <article className="order-summary">
+      <h2 className="order-summary-heading">Order Summary</h2>
+      <section className="price-breakdown">
+        <div>
+          Subtotal <span>${subtotal}</span>
+        </div>
+        <div>
+          Total Tax <span>${totalTax}</span>
+        </div>
+        <div>
+          Total <span>${total}</span>
+        </div>
+      </section>
+      <StripeCheckoutButton price={total} label="Pay Now" />
+    </article>
+  );
+};
