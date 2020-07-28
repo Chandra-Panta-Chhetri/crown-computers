d98e0a93f5bf434177ac82c3bb4e840c13787598
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-07-28 11:50:48 -0400

    Converts login class comp to functional using hooks.

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { LoginContainer } from "./login.styles";
 import {
   FormContainer,
@@ -19,65 +19,69 @@ import {
 import { connect } from "react-redux";
 import { selectAuthError } from "../../redux/user/user.selectors";
 
-class LogIn extends React.Component {
-  state = {
+const LogIn = ({
+  startGoogleSignIn,
+  loginErrorMsg,
+  clearAuthError,
+  startEmailSignIn
+}) => {
+  const [userCredentials, setUserCredentials] = useState({
     email: "",
     password: ""
-  };
+  });
 
-  componentDidMount() {
-    const { clearAuthError } = this.props;
+  useEffect(() => {
     clearAuthError();
-  }
+  }, [clearAuthError]);
 
-  storeCredentials = (e) => this.setState({ [e.target.name]: e.target.value });
+  const { email, password } = userCredentials;
 
-  loginUser = (e) => {
-    e.preventDefault();
-    const { email, password } = this.state;
-    const { startEmailSignIn } = this.props;
-    startEmailSignIn({ email, password });
+  const storeCredentials = (e) => {
+    const { value, name } = e.target;
+    setUserCredentials({ ...userCredentials, [name]: value });
   };
 
-  render() {
-    const { startGoogleSignIn, loginErrorMsg } = this.props;
-    return (
-      <LoginContainer>
-        <FormContainer>
-          <FormTitle>LOGIN</FormTitle>
-          <ErrorText>{loginErrorMsg}</ErrorText>
-          <Form onSubmit={this.loginUser}>
-            <FormInput
-              type={"email"}
-              name={"email"}
-              label={"Email"}
-              value={this.state.email}
-              handler={this.storeCredentials}
-              required
-            />
-            <FormInput
-              type={"password"}
-              name={"password"}
-              label={"Password"}
-              value={this.state.password}
-              handler={this.storeCredentials}
-              required
-            />
-            <FormButton type="submit">Log In</FormButton>
-            <h5>Or login with</h5>
-            <FormButton type="button" onClick={startGoogleSignIn}>
-              <i className="fab fa-google"></i> Google
-            </FormButton>
-          </Form>
-          <h5>
-            Don't have an account?{" "}
-            <FormRedirectLink to="/signup">Sign up now</FormRedirectLink>
-          </h5>
-        </FormContainer>
-      </LoginContainer>
-    );
-  }
-}
+  const loginUser = (e) => {
+    e.preventDefault();
+    startEmailSignIn(userCredentials);
+  };
+
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <FormTitle>LOGIN</FormTitle>
+        <ErrorText>{loginErrorMsg}</ErrorText>
+        <Form onSubmit={loginUser}>
+          <FormInput
+            type={"email"}
+            name={"email"}
+            label={"Email"}
+            value={email}
+            handler={storeCredentials}
+            required
+          />
+          <FormInput
+            type={"password"}
+            name={"password"}
+            label={"Password"}
+            value={password}
+            handler={storeCredentials}
+            required
+          />
+          <FormButton type="submit">Log In</FormButton>
+          <h5>Or login with</h5>
+          <FormButton type="button" onClick={startGoogleSignIn}>
+            <i className="fab fa-google"></i> Google
+          </FormButton>
+        </Form>
+        <h5>
+          Don't have an account?{" "}
+          <FormRedirectLink to="/signup">Sign up now</FormRedirectLink>
+        </h5>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
 
 const mapStateToProps = (state) => ({
   loginErrorMsg: selectAuthError(state)
