1b4fa45f49ba71ea71d06f8d49a21c5c69399ccd
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-08-10 20:37:12 -0400

    Redirects all http requests to https. Creates endpoint to send service-worker.js file.

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const compression = require("compression");
+const enforce = require("express-sslify");
+const path = require("path");
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(compression());
 app.use(express.json());
 
@@ -15,6 +18,10 @@ app.post("/checkout", async (req, res) => {
   } catch (e) {
     res.status(400).send({ msg: e.message });
   }
+});
+
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/service-worker.js"));
 });
 
 module.exports = app;
