7c909ac982b5c77203d72221a44ab6537913695c
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-08-16 19:46:27 -0400

    Fixes styling issue where long toast message getting cut off. Shortens add to cart notification message.

--- client/src/redux/notification/notification.sagas.js
+++ client/src/redux/notification/notification.sagas.js
@@ -2,13 +2,9 @@ import { takeEvery, call, all, put } from "redux-saga/effects";
 import CART_ACTION_TYPES from "../cart/cart.action.types";
 import { addSuccessNotification } from "./notification.actions";
 
-function* showSuccessNotification({ payload: { id, name } }) {
+function* showSuccessNotification({ payload: { id, name, price } }) {
   yield put(
-    addSuccessNotification(
-      id,
-      "Added To Cart",
-      `${name} was successfully added to your cart!`
-    )
+    addSuccessNotification(id, "Added To Cart", `${name} ($${price} ea.)`)
   );
 }
 
