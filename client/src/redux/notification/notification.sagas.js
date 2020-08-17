2792e608941f70b24d98e775785bb01eb7bf861d
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-08-17 16:04:53 -0400

    Adds saga to watch for remove from cart events and display removed from cart success notification.

--- client/src/redux/notification/notification.sagas.js
+++ client/src/redux/notification/notification.sagas.js
@@ -2,16 +2,29 @@ import { takeEvery, call, all, put } from "redux-saga/effects";
 import CART_ACTION_TYPES from "../cart/cart.action.types";
 import { addSuccessNotification } from "./notification.actions";
 
-function* showSuccessNotification({ payload: { id, name, price } }) {
+function* showAddedToCartNotification({ payload: { id, name, price } }) {
   yield put(
     addSuccessNotification(id, "Added To Cart", `${name} ($${price} ea.)`)
   );
 }
 
+function* showRemovedFromCartNotification({ payload: { id, name, price } }) {
+  yield put(
+    addSuccessNotification(id, "Removed From Cart", `${name} ($${price} ea.)`)
+  );
+}
+
 function* watchAddToCart() {
-  yield takeEvery(CART_ACTION_TYPES.ADD_TO_CART, showSuccessNotification);
+  yield takeEvery(CART_ACTION_TYPES.ADD_TO_CART, showAddedToCartNotification);
+}
+
+function* watchRemoveFromCart() {
+  yield takeEvery(
+    CART_ACTION_TYPES.REMOVE_FROM_CART,
+    showRemovedFromCartNotification
+  );
 }
 
 export default function* notificationSagas() {
-  yield all([call(watchAddToCart)]);
+  yield all([call(watchAddToCart), call(watchRemoveFromCart)]);
 }
