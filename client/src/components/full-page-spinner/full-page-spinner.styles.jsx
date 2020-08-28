4153681af9985706cfed30c39f2612d100654c5f
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-08-27 20:35:50 -0400

    Refactors spinner styles and full page spinner styles by spliting common css into variables.

--- client/src/components/full-page-spinner/full-page-spinner.styles.jsx
+++ client/src/components/full-page-spinner/full-page-spinner.styles.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import {
+  spinAnimationKeyframe,
+  loadingDotsAnimationStyles,
+  SpinnerTextStyles,
+  SpinnerStyles
+} from "../spinner/spinner.styles";
 
 export const FullPageSpinnerContainer = styled.div`
   position: fixed;
@@ -11,18 +17,6 @@ export const FullPageSpinnerContainer = styled.div`
   z-index: 9999;
   visibility: ${(props) => (props.isLoading ? "visible" : "hidden")};
 
-  @keyframes spin {
-    to {
-      -webkit-transform: rotate(360deg);
-    }
-  }
-
-  @-webkit-keyframes spin {
-    to {
-      -webkit-transform: rotate(360deg);
-    }
-  }
-
   &::after {
     content: "";
     display: block;
@@ -30,20 +24,13 @@ export const FullPageSpinnerContainer = styled.div`
     margin-left: -25px;
     left: 50%;
     top: 40%;
-    width: 50px;
-    height: 50px;
-    border: 4px solid rgba(195, 195, 195, 0.8);
-    border-radius: 50%;
-    border-top-color: #636767;
-    animation: spin 1s ease-in-out infinite;
-    -webkit-animation: spin 1s ease-in-out infinite;
+    ${SpinnerStyles}
   }
+  ${spinAnimationKeyframe}
 `;
 
 export const FullPageSpinnerText = styled.p`
-  font-size: 18px;
-  font-weight: 600;
-  color: black;
+  ${SpinnerTextStyles}
   z-index: 9999;
   position: absolute;
   left: 50%;
@@ -52,28 +39,5 @@ export const FullPageSpinnerText = styled.p`
   width: 170px;
   height: 24px;
   text-align: center;
-
-  &:after {
-    content: " .";
-    animation: dots 1s steps(5, end) infinite;
-  }
-
-  @keyframes dots {
-    0%,
-    20% {
-      color: rgba(0, 0, 0, 0);
-      text-shadow: 0.25em 0 0 rgba(0, 0, 0, 0), 0.5em 0 0 rgba(0, 0, 0, 0);
-    }
-    40% {
-      color: black;
-      text-shadow: 0.25em 0 0 rgba(0, 0, 0, 0), 0.5em 0 0 rgba(0, 0, 0, 0);
-    }
-    60% {
-      text-shadow: 0.25em 0 0 black, 0.5em 0 0 rgba(0, 0, 0, 0);
-    }
-    80%,
-    100% {
-      text-shadow: 0.25em 0 0 black, 0.5em 0 0 black;
-    }
-  }
+  ${loadingDotsAnimationStyles}
 `;
--- client/src/components/spinner/spinner.styles.jsx
+++ client/src/components/spinner/spinner.styles.jsx
@@ -1,24 +1,6 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const SpinnerOverlay = styled.div`
-  height: 85vh;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-export const SpinnerContainer = styled.div`
-  display: inline-block;
-  width: 50px;
-  height: 50px;
-  border: 4px solid rgba(195, 195, 195, 0.8);
-  border-radius: 50%;
-  border-top-color: #636767;
-  animation: spin 1s ease-in-out infinite;
-  -webkit-animation: spin 1s ease-in-out infinite;
-
+export const spinAnimationKeyframe = css`
   @keyframes spin {
     to {
       -webkit-transform: rotate(360deg);
@@ -32,17 +14,11 @@ export const SpinnerContainer = styled.div`
   }
 `;
 
-export const SpinnerText = styled.p`
-  font-size: 18px;
-  margin-top: 10px;
-  font-weight: 600;
-  color: black;
-
+export const loadingDotsAnimationStyles = css`
   &:after {
     content: " .";
     animation: dots 1s steps(5, end) infinite;
   }
-
   @keyframes dots {
     0%,
     20% {
@@ -61,4 +37,41 @@ export const SpinnerText = styled.p`
       text-shadow: 0.25em 0 0 black, 0.5em 0 0 black;
     }
   }
+`;
+
+export const SpinnerTextStyles = css`
+  font-size: 18px;
+  font-weight: 600;
+  color: black;
+`;
+
+export const SpinnerStyles = css`
+  width: 50px;
+  height: 50px;
+  border: 4px solid rgba(195, 195, 195, 0.8);
+  border-radius: 50%;
+  border-top-color: #636767;
+  animation: spin 1s ease-in-out infinite;
+  -webkit-animation: spin 1s ease-in-out infinite;
+`;
+
+export const SpinnerOverlay = styled.div`
+  height: 85vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const SpinnerContainer = styled.div`
+  display: inline-block;
+  ${SpinnerStyles}
+  ${spinAnimationKeyframe}
+`;
+
+export const SpinnerText = styled.p`
+  ${SpinnerTextStyles}
+  margin-top: 10px;
+  ${loadingDotsAnimationStyles}
 `;
