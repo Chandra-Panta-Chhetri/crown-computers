7037f57553caba1b18ec548647ba546a88018901
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-10-20 10:12:08 -0400

    Improves readability of cloud functions. Updates database schemas to add new sales_summary collection.

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -2,28 +2,37 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+const salesSummaryCollectionRef = admin.firestore().collection("sales_summary");
+
+const getPreviousSaleSummaryDoc = async () => {
+  const salesSummarySnapshot = await salesSummaryCollectionRef.limit(1).get();
+  return salesSummarySnapshot.docs[0];
+};
+
+const createNewSaleSummary = async (saleSummaryInfo) =>
+  await salesSummaryCollectionRef.add(saleSummaryInfo);
+
+const updateSaleSummary = async (updatedSummaryInfo, saleSummaryDocRef) =>
+  await saleSummaryDocRef.update(updatedSummaryInfo);
+
 exports.onNewSaleCreated = functions.firestore
   .document("/sales/{id}")
   .onCreate(async (snap, context) => {
     const { subTotal, itemsSold } = snap.data();
     const numProductsSold = itemsSold.length;
-    const salesSummaryCollectionRef = admin
-      .firestore()
-      .collection("sales_summary");
-    const getPreviousSalesSummary = salesSummaryCollectionRef.limit(1);
-    const salesSummarySnapshot = await getPreviousSalesSummary.get();
-    const saleSummaryDocSnapshot = salesSummarySnapshot.docs[0];
-    if (!saleSummaryDocSnapshot) {
-      console.log("creating new sale summary");
-      return await salesSummaryCollectionRef.add({
+    const previousSaleSummaryDoc = await getPreviousSaleSummaryDoc();
+    if (!previousSaleSummaryDoc) {
+      return await createNewSaleSummary({
         salesTotal: subTotal,
         totalProductsSold: numProductsSold
       });
     }
-    const { salesTotal, totalProductsSold } = saleSummaryDocSnapshot.data();
-    console.log(saleTotal, totalProductsSold, "Updating sale summary");
-    return await saleSummaryDocSnapshot.ref.update({
-      salesTotal: salesTotal + subTotal,
-      totalProductsSold: totalProductsSold + numProductsSold
-    });
+    const { salesTotal, totalProductsSold } = previousSaleSummaryDoc.data();
+    return await updateSaleSummary(
+      {
+        salesTotal: salesTotal + subTotal,
+        totalProductsSold: totalProductsSold + numProductsSold
+      },
+      previousSaleSummaryDoc.ref
+    );
   });
