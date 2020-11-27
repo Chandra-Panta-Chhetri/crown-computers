18f06dc80dbd569cc5bdeec82fe02ee839269bd3
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-11-26 19:13:02 -0500

    Creates basic theme-toggler component.

---
+++ client/src/components/theme-toggler/theme-toggler.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ThemeTogglerContainer } from "./theme-toggler.styles";
+
+const ThemeToggler = () => (
+  <ThemeTogglerContainer>
+    <span>theme toggler</span>
+  </ThemeTogglerContainer>
+);
+
+export default ThemeToggler;
