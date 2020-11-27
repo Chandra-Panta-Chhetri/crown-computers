4ce74b119239c3f6aebb6e4c72a14c5b8cb602c7
Chandra Panta Chhetri <chandra.panta345@hotmail.com>
2020-11-26 19:35:12 -0500

    Persists theme even after page refresh.

--- client/src/redux/theme/theme.actions.js
+++ client/src/redux/theme/theme.actions.js
@@ -1,6 +1,6 @@
 import THEME_ACTION_TYPES from "./theme.action.types";
 
-export const changeTheme = (theme) => ({
+export const changeTheme = (themeName) => ({
   type: THEME_ACTION_TYPES.CHANGE_THEME,
-  payload: theme
+  payload: themeName
 });
--- client/src/redux/theme/theme.reducer.js
+++ client/src/redux/theme/theme.reducer.js
@@ -1,16 +1,30 @@
 import THEME_ACTION_TYPES from "./theme.action.types";
-import { LIGHT_THEME } from "../../theme";
+import { DARK_THEME, LIGHT_THEME } from "../../theme";
+
+const THEME_NAME_THEME_MAP = {
+  light: LIGHT_THEME,
+  dark: DARK_THEME
+};
+
+const persistedThemeName = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : "light";
+
+const persistedTheme = THEME_NAME_THEME_MAP[persistedThemeName]
+  ? THEME_NAME_THEME_MAP[persistedThemeName]
+  : LIGHT_THEME;
 
 const INITIAL_STATE = {
-  theme: LIGHT_THEME
+  theme: persistedTheme
 };
 
 const themeReducer = (prevState = INITIAL_STATE, action) => {
   switch (action.type) {
     case THEME_ACTION_TYPES.CHANGE_THEME:
+      localStorage.setItem("theme", action.payload);
       return {
         ...prevState,
-        theme: action.payload
+        theme: THEME_NAME_THEME_MAP[action.payload]
       };
     default:
       return prevState;
